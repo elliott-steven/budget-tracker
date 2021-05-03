b7f5727f855c8f33d4211ea10f7626e66779dcb2
Steven Elliott <steven.elliott@live.com>
2021-05-03 17:24:04 -0400

    added service worker to html

---
+++ public/serviceworker.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = 'static-cache';
+const DATA_CACHE_NAME = 'data-cache';
+
+const staticFilesToPreCache = [
+  "/",
+  "./icons/icon-192x192.png",
+  "./icons/icon-512x512.png",
+  "index.html",
+  "index.js",
+  "styles.css",
+  "/manifest.webmanifest",
+  "db.js",
+  "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+  "https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
+];
+
