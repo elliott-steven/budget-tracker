849689f9d1f4b56558b304fa745a9c86fa5912fd
Steven Elliott <steven.elliott@live.com>
2021-05-03 16:38:18 -0400

    finish database code and added db.js script to html

--- public/db.js
+++ public/db.js
@@ -20,8 +20,58 @@ request.onsuccess = function (event) {
     }
 };
 
-request.onerror = function (event){
+request.onerror = function (event) {
 
     console.log("Error Occured!");
 };
 
+function saveRecord(record) {
+    //this will create a transaction on the pending db
+    const transaction = db.transaction(["pending"], "readwrite");
+
+    //accesses the pending object store
+    const store = transaction.objectStore("pending");
+
+    //adds the record
+    store.add(record);
+}
+
+function checkDB() {
+
+    const transaction = db.transaction(["pending"], "readwrite");
+
+    const store = transaction.objectStore("pending");
+
+    //gets all records from object store and set to a getAll variable
+    const getAll = store.getAll();
+
+    getAll.onsuccess = function() {
+
+        if (getAll.result.length > 0) {
+            
+            fetch("api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "applicaion/json, text/plain, */*", 
+                    "Content-Type": "application/json",
+                },
+            })
+
+            .then((response) => response.json())
+
+            .then(() => {
+
+                const transaction = db.transaction(["pending"], "readwrite");
+
+                const store = transaction.objectStore("pending");
+
+                //clears all items from store after success
+                store.clear();
+            });
+        }
+    };
+}
+
+//event listener is waiting for app to come back online
+window.addEventListener("online", checkDB);
